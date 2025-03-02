263ed9797e0de19345f6531f546ff8a8ef678433
José Ricardo <josericardogithub@gmail.com>
2025-03-01 21:58:43 -0300

    aula14 ex017

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,14 +1,9 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var hatual = new Date()
-    var hora = hatual.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora <= 12) {
-        img.src = 'fotomanha.jpg'
-    }   else if (hora > 12 && hora < 18) {
-        img.src = 'fototarde.jpg'
-    }   else {
-        img.src = 'fotonoite.jpg'
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let res = document.getElementById('res')
+    let n = Number(num.value)
+    res.innerHTML = ''
+    for (var c = 1; c <= 10; c +=1) {
+        res.innerHTML += `${n} x ${c} = ${n*c}<br>`
     }
 }
